9250df8b755a0f8f97002e1438ec6ea94c3074b3
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-02-20 14:43:56 +0530

    deployment changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -22,7 +22,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "http://localhost:5000";
+  const ENDPOINT = "https://shivamkakkar-chat-app.herokuapp.com/";
   const history = useHistory();
 
   useEffect(() => {
